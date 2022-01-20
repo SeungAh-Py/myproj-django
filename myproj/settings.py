48d3af4f9ef98f4eead95b79f4c115bac62f5367
SeungAh Kim <cpuasdnwin@naver.com>
2022-01-20 17:30:54 +0900

    settings 오류 수정

--- myproj/settings.py
+++ myproj/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env.str("SECRET_KEY", default="---- SECRET KEY ----")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOST", default=[])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
 
 # Application definition
