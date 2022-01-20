1b0726ba4241e24f773d533d1a8984d400936633
SeungAh Kim <cpuasdnwin@naver.com>
2022-01-20 13:15:39 +0900

    220120 settings

--- myproj/urls.py
+++ myproj/urls.py
@@ -18,12 +18,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
     path('news/', include('news.urls')),
     path('shop/', include("shop.urls")),
-    path('ititems/', include('ititems.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
